fad72434e980e09420b5bbf3b777d1da4bfdca6e
HwangDongHyeon <smurp741@naver.com>
2023-07-06 14:51:10 +0900

    커밋3 곱하기 함수 추가

--- mail.py
+++ mail.py
@@ -5,6 +5,10 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def sub_func(n1,n2):
+    return n1*n2
+
+
 ## 전역 변수부
 num1,num2,result = 100,200,0
 
@@ -15,3 +19,6 @@ print( num1, '+', num2, '=', result)
 
 result = add_func(num1, num2)
 print( num1, '-', num2, '=', result)
+
+result = add_func(num1, num2)
+print( num1, '*', num2, '=', result)
